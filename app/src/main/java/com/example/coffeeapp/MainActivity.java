2b8e6ee90060aad0e14d9adb0b3284af401621f6
the_7th_cavalry <Max99pain@rambler.ru>
2020-11-14 16:14:05 +0300

    add data retrieving from api

--- app/src/main/java/com/example/coffeeapp/MainActivity.java
+++ app/src/main/java/com/example/coffeeapp/MainActivity.java
@@ -10,10 +10,22 @@ import android.view.View;
 import android.widget.TextView;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MainRecyclerViewAdapter.ItemClickListener {
     MainRecyclerViewAdapter adapter;
+    static HttpURLConnection connection;
 
 
 
@@ -42,18 +54,71 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
             }
         });
 
-        // data to populate the RecyclerView with
-        ArrayList<String> coffeeNames = new ArrayList<>();
-        coffeeNames.add("Эспрессо");
-        coffeeNames.add("Капучино");
-        coffeeNames.add("Американо");
-        coffeeNames.add("Латте");
-        coffeeNames.add("Фраппе");
+        final List<String> coffeeNames = Collections.synchronizedList(new ArrayList<String>());
 
-        //TODO get data from JSON here
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BufferedReader bufferedReader;
+                String line;
+                StringBuffer responseContent = new StringBuffer();
+                boolean requestIsOk = true;
 
 
+                try {
+                    URL url = new URL("http://testapi.servertest.pro/api/objects/");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(5000);
+                    connection.setReadTimeout(5000);
+                    connection.setRequestProperty("Authorization", "Token d23d2a771294e285257976bc1e5a4040fa6c14c8");
+                    connection.connect();
 
+                    int status = connection.getResponseCode();
+
+                    if (status > 299) {
+                        requestIsOk = false;
+                        bufferedReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                        while ((line = bufferedReader.readLine()) != null) {
+                            responseContent.append(line);
+                        }
+                        bufferedReader.close();
+                    }
+                    else {
+                        bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        while ((line = bufferedReader.readLine()) != null) {
+                            responseContent.append(line);
+                        }
+                        bufferedReader.close();
+                    }
+
+                    System.out.println(responseContent.toString());
+
+                    if (requestIsOk) {
+                        JSONObject entireJsonObject = new JSONObject(responseContent.toString());
+                        JSONArray jsonCoffeeNames = entireJsonObject.getJSONArray("results");
+                        for (int i = 0; i < jsonCoffeeNames.length(); i++) {
+                            JSONObject jsonObject = jsonCoffeeNames.getJSONObject(i);
+                            String coffeeName = jsonObject.getString("title");
+                            coffeeNames.add(coffeeName);
+                        }
+                    }
+
+
+                    connection.disconnect();
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         // set up the RecyclerView
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MainRecyclerViewAdapter(this, coffeeNames);
